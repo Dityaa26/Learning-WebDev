10d3b1fa6128688b59260b0f274ea08475a14e08
Aditya Sharma <aditya261862@gmail.com>
2023-07-18 19:17:28 +0530

    JS equality

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,10 @@
 // JavaScript Equality
 
 const num1 = 6;
-const num2 = '6';
+const num2 = 10;
 
 const value = num1 == num2;
 const value2 = num1 === num2
 
-console.log(value)
-console.log(value2)
+console.log(value) //false
+console.log(value2) //false
