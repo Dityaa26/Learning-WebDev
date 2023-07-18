483cc358f835a4c7daf93b3b43c4606df4f16df0
Aditya Sharma <aditya261862@gmail.com>
2023-07-18 18:50:31 +0530

    JavaScript function expression

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+function Add(num1,num2){
+    return num1+num2;
+}
+
+let n = Add(2,3);
+console.log(n)
+
+// Function Expression
+
+const add = function (num1,num2){
+    return num1+num2;
+}
+
+console.log(add(2,4))
