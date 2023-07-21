6401ca48d35661f186ee7504a5acfa6a7a0274d3
Aditya Sharma <aditya261862@gmail.com>
2023-07-21 12:43:12 +0530

    Type Conversion

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,32 @@
-// JavaScript Equality
+let value = 'a'-'b'
+console.log("value : " + value)
 
-const num1 = 6;
-const num2 = '6';
+let x = 'a' + 4
+console.log("x : " + x)
 
-const value = num1 != num2;
-const value2 = num1 !== num2
+let y = 4 + 'a'
+console.log("y : " + y)
 
-console.log(value) //false
-console.log(value2) //true
+let z = 4 * 'a'
+console.log("z : " + z)
+
+let p = 'a' * 4
+console.log("p : " + p)
+
+let q = 'a' - 4
+console.log("q : " + q)
+
+let r = 4 - 'a'
+console.log("r : " + r)
+
+let s = '10' - 'a'
+console.log("s : " + s)
+
+let t = '10' * 'a'
+console.log("t : " + t)
+
+let u = '10' * '10'
+console.log('u : ' + u)
+
+let v = "10" / "10";
+console.log("v : " + v);
