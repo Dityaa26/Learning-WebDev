61a8e4cb2c2dc4a15ead87a706ed0c11d034c120
Aditya Sharma <aditya261862@gmail.com>
2023-07-23 09:39:36 +0530

    Callback Function

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,16 +1,17 @@
-// Strings
+// Callback function
 
-let text = 'String variable'
+function morning(name){
+    return `Good morning ${name}`
+}
 
-console.log(text.length)
-console.log(text.toUpperCase())
-console.log(text.toLowerCase())
-console.log(text[2])
-console.log(text.charAt(5).toUpperCase())
-console.log(text)
-console.log(text.indexOf('ria'))
-console.log(text.indexOf("rin"));
-console.log(text.includes(' var'))
-console.log(text.includes('rav'))
-console.log(text.slice(0,2))
-console.log(text.slice(-2))
+function afternoon(name) {
+  return `Good afternoon ${name}`;
+}
+
+function greet(name, cbf){
+    const myName = 'Aditya'
+    console.log(`${cbf(name)}, my name is ${myName}`)
+}
+
+greet('abhinav', morning)
+greet('aakash', afternoon)
