d2f17c18b93b59f294a85a02cde887f794a9ed41
Aditya Sharma <aditya261862@gmail.com>
2023-08-07 15:54:10 +0530

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,10 @@
+document.body.style.backgroundColor = 'blanchedalmond';
+document.body.style.color = "blue";
+document.getElementById('btn').style.color = 'red';
+
+const button = document.getElementById('btn')
+
+button.style.color = 'yellow';
+
+
+console.log(button.style)
